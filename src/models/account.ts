baf9bdd0362dc8ad6ed7dc43f8713f818c36f234
Yewon Kim <37951612+kyw017763@users.noreply.github.com>
2020-03-02 00:08:37 +0900

    Remove unused statics method in mongoose schema

--- src/models/account.ts
+++ src/models/account.ts
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 import { connection } from './Database';
 import bcrypt from 'bcryptjs';
-import config from '../config';
 
 export interface IAccount extends mongoose.Document {
   [x: string]: any;
@@ -61,50 +60,5 @@ accountSchema.pre<IAccount>('save', function (next) {
 accountSchema.methods.compareHash = function (pwd: string) {
   return bcrypt.compareSync(pwd, this.pwd);
 }
-
-accountSchema.statics = {
-  createAccount: function (id: string, name: string, pwd: string, sNum: number, interest1: string, interest2: string, interest3: string, profile: string, verifyKey: string) {
-    return this.create({
-      id, name, pwd, sNum, interest1, interest2, interest3, profile, verifyKey,
-    });
-  },
-  getAccountById: function (userId: string) {
-    return this.find({ id: userId });
-  },
-  updateAccount: function (userId: string, name: string, sNum: number, interest1: string, interest2: string, interest3: string, profile: string) {
-    return this.findOneAndUpdate({ id: userId }, {
-      $set: {
-        name, sNum, interest1, interest2, interest3, profile,
-      },
-    });
-  },
-  updatePwd: function (userId: string, newPwd: string) {
-    const pwd = this.find({ id: userId }).select('pwd');
-
-    if (pwd === newPwd) {
-      return false;
-    // eslint-disable-next-line no-else-return
-    } else {
-      this.findOneAndUpdate(
-        { id: userId },
-        { $set: { pwd: newPwd } },
-      );
-      return true;
-    }
-  },
-  updateNoti: function (userId: string, notiApply: number, notiRecv: number, notiVol: number) {
-    return this.findOneAndUpdate(
-      { id: userId },
-      {
-        $set: {
-          notiApply, notiRecv, notiVol,
-        },
-      },
-    );
-  },
-  secession: function (userId: string) {
-    return this.findOneAndUpdate({ id: userId }, { active: false });
-  },
-};
 
 export const Account: mongoose.Model<IAccount> = connection.model<IAccount>('Account', accountSchema);
