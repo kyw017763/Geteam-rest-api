1fdcfdb012992c81206561322710e0778e714be9
Yewon Kim <37951612+kyw017763@users.noreply.github.com>
2020-03-12 21:14:42 +0900

    Add email template

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -2,6 +2,7 @@ import nodemailer from 'nodemailer';
 import config from './../config';
 import { IStudy } from '../models/study';
 import { IContest } from '../models/contest';
+import getEmailForm from './getEmailForm';
 
 export async function sendAuthEmail(userEmail: string, key: string) {
   try {
@@ -14,19 +15,19 @@ export async function sendAuthEmail(userEmail: string, key: string) {
         pass: process.env.PWD || config.PWD,
       },
     });
+
+    const emailContent = `
+    Geteam 계정에 등록하신 이메일 주소(${userEmail})가 올바른지 확인하기 위한 이메일입니다.<br>
+    아래의 버튼을 클릭하여 이메일 인증을 완료해주세요!<br>
+    개인정보 보호를 위해 인증 링크는 하루동안만 유효합니다.<br>
+    만약 인증 메일의 재발송을 원하신다면 <a href="http://127.0.0.1:3003/signup/verify/new/${key}?email=${userEmail}" style="color: #efdc05;">이 링크</a>를 클릭해주세요!
+    `;
   
     const mailOptions = {
       from: process.env.EMAIL || config.EMAIL,
       to: userEmail,
       subject: 'Geteam 이메일 인증',
-      html: `<h3>Geteam 이메일 인증용 링크</h3>
-      Geteam 계정에 등록하신 이메일 주소(${userEmail})가 올바른지 확인하기 위한 인증 링크입니다.
-      이 <a href="http://127.0.0.1:3003/signup/verify/${key}" style="color: #efdc05;">인증 링크</a>를 클릭하여 이메일 인증을 완료해 주세요!
-      <br>
-      개인정보 보호를 위해 인증 링크는 하루동안만 유효합니다.
-      <br>
-      만약 인증 메일의 재발송을 원하신다면 <a href="http://127.0.0.1:3003/signup/verify/new/${key}?email=${userEmail}" style="color: #efdc05;">이 링크</a>를 클릭해주세요!
-      `,
+      html: getEmailForm('Geteam 이메일 인증용 링크', emailContent, '이메일 인증 완료', `http://127.0.0.1:3003/signup/verify/${key}`),
     };
 
     const info = await transporter.sendMail(mailOptions);
@@ -49,14 +50,16 @@ export async function sendPwdEmail(subject: string, userEmail: string, userName:
         pass: process.env.PWD || config.PWD,
       },
     });
+
+    const emailContent = `${userName}님의 임시 비밀번호는 <span style="background: #efdc05;">${userTempPwd}</span> 입니다.`;
   
     const mailOptions = {
       from: process.env.EMAIL || config.EMAIL,
       to: userEmail,
       subject: subject,
-      html: `<h1>비밀번호 초기화</h1> ${userName}님의 임시 비밀번호는 <span style="background: #efdc05;">${userTempPwd}</span> 입니다.`,
+      html: getEmailForm('Geteam 비밀번호 초기화', emailContent, 'Geteam 바로가기', `http://127.0.0.1:3003/signin`),
     };
-  
+
     const info = await transporter.sendMail(mailOptions);
     console.log(`Message sent : ${info.response}`);
     transporter.close();
@@ -108,15 +111,16 @@ export async function sendTeamEmail (kind: string, item: IStudy | IContest, cont
         pass: process.env.PWD || config.PWD,
       },
     });
-  
-    let message = content;
-    message = message.replace(/(?:\r\n|\r|\n)/g, '<br />');
+
     
+    const message = content.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    const emailContent = `<h5>팀 모집자님(${item.account})이 보내셨습니다<h5><br>${message}`;
+  
     const mailOptions = {
       from: process.env.EMAIL || config.EMAIL,
       to: process.env.EMAIL || config.EMAIL,
-      subject: `Geteam 팀 모집 완료 : ${item.title}`,
-      html: `<h3>[${kind}] ${item.title}</h3><br /> <h5>팀 모집자님(${item.account})이 보내셨습니다<h5><br />${message}`,
+      subject: `Geteam 팀 모집 완료 : [${kind}] ${item.title}`,
+      html: getEmailForm(`Geteam 팀 모집 완료`, emailContent, '모집글 바로가기', `http://127.0.0.1:3003/board/<${kind}/${item._id}`),
     };
   
     const info = await transporter.sendMail(mailOptions);
